ecfc300b29edd09afc54ec7256a4337b48a99501
daijunxiang <2742840352@qq.com>
2022-11-02 16:25:53 +0800

    2022年11月2日16:25:47

---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/PurchaseDetailService.java
@@ -0,0 +1,20 @@
+package com.atguigu.gulimall.ware.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.common.utils.PageUtils;
+import com.atguigu.gulimall.ware.entity.PurchaseDetailEntity;
+
+import java.util.Map;
+
+/**
+ * 
+ *
+ * @author djx
+ * @email djx@gmail.com
+ * @date 2022-11-02 16:07:55
+ */
+public interface PurchaseDetailService extends IService<PurchaseDetailEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+}
+
