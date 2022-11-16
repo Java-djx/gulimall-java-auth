20ed7c0b6fe13344aa2b0ccde30659efbcf04192
daijunxiang <2742840352@qq.com>
2022-11-16 22:48:39 +0800

    2022年11月14日21:02:08

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/config/GulimallWebConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @version 1.0
+ * @author: djx
+ * @createTime: 2022/11/16 22:48
+ */
+@Configuration
+public class GulimallWebConfig implements WebMvcConfigurer {
+
+    /**·
+     * 视图映射:发送一个请求，直接跳转到一个页面
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        // registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/reg.html").setViewName("reg");
+    }
+}
---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.atguigu.gulimall.auth.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @version 1.0
+ * @author: djx
+ * @createTime: 2022/11/16 22:21
+ */
+@Controller
+@Slf4j
+public class LoginController {
+
+    @GetMapping({"/login.html","/"})
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping({"/reg.html"})
+    public String pageGotoReg(){
+        return "reg";
+    }
+
+}
